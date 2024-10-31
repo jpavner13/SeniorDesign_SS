7ac1f86def4a9c995c00c9c94d84a5f02a7e574a
Pierce Avner <jpavner@gmail.com>
2024-10-31 21:36:59 +0000

    Changed tkinter to flask

--- src/GUI/GUI_Skelaton.py
+++ src/GUI/GUI_Skelaton.py
@@ -1,38 +1,68 @@
-import tkinter as tk
-import os
- 
-class DronesGui:  # Blueprint of our GUI, Class.
-    def __init__(self, master):
-        self.master = master  # Store the master window reference
-        self.master.title("SS DRONES GUI")
-        self.master.geometry("600x200")
-        self.master.configure(bg="black")
- 
-        myFrame = tk.Frame(self.master, bg="grey")  # Reference Frame as tk.Frame
-        myFrame.pack(pady=20)  # Add some padding for aesthetics
- 
-        # self.label = tk.Label(myFrame, text="Welcome to SS Drones!", font=("Times New Roman", 16))  # Reference Label as tk.Label
-        # self.label.pack(pady=10)
- 
-        self.blackout_button = tk.Button(myFrame, text="BLACKOUT", command=self.Blackout, bg="red", fg="white")  # Reference Button as tk.Button
-        self.blackout_button.pack(pady=10)
- 
-        self.brownout_button = tk.Button(myFrame, text="BROWNOUT", command=self.Brownout, bg="yellow", fg="white")  # Reference Button as tk.Button
-        self.brownout_button.pack(pady=10)
- 
-    def Blackout(self):
-        # TODO: Will need specific code to disconnect Battery from Larger Assembly
-        print("Blackout Button Triggered")  
-    
-    def Brownout(self):
-        # TODO: Will need specific code to clear pending and current commands. Drone will be hovering
-        print("Brownout Button Triggered")
- 
- 
-root = tk.Tk()
-# icon_path = os.path.join(os.getcwd(), "SS_logo.png")
-# icon = tk.PhotoImage(file=icon_path)
-# root.wm_iconphoto(True, icon)
- 
-Drones_gui_object = DronesGui(root)  # Instance of Drones GUI Class, Object.
-root.mainloop()
+from flask import Flask, render_template_string
+
+app = Flask(__name__)
+
+# Define the HTML template as a string
+HTML_TEMPLATE = '''
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>SS DRONES GUI</title>
+    <style>
+        body {
+            background-color: black;
+            color: white;
+            font-family: Arial, sans-serif;
+            display: flex;
+            flex-direction: column;
+            align-items: center;
+            justify-content: center;
+            height: 100vh;
+            margin: 0;
+        }
+        .button {
+            padding: 15px 30px;
+            font-size: 16px;
+            margin: 10px;
+            border: none;
+            border-radius: 5px;
+            cursor: pointer;
+        }
+        .blackout {
+            background-color: red;
+            color: white;
+        }
+        .brownout {
+            background-color: yellow;
+            color: black;
+        }
+    </style>
+</head>
+<body>
+    <h1>SS DRONES GUI</h1>
+    <button onclick="location.href='/blackout'" class="button blackout">BLACKOUT</button>
+    <button onclick="location.href='/brownout'" class="button brownout">BROWNOUT</button>
+</body>
+</html>
+'''
+
+@app.route('/')
+def index():
+    return render_template_string(HTML_TEMPLATE)
+
+@app.route('/blackout')
+def blackout():
+    # TODO: Will need specific code to disconnect Battery from Larger Assembly
+    print("Blackout Button Triggered")
+    return index()  # Redirect to the main page after the action
+
+@app.route('/brownout')
+def brownout():
+    # TODO: Will need specific code to clear pending and current commands. Drone will be hovering
+    print("Brownout Button Triggered")
+    return index()  # Redirect to the main page after the action
+
+if __name__ == '__main__':
+    app.run(debug=True)
